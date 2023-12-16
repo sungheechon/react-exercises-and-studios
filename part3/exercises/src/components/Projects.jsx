f92a9030866337b7fca19e53a5f45a3ec1628bd1
sunghee chon <sungheechon@sunghees-imac.lan>
2023-12-16 09:00:20 -0500

    CH 31: Reat, Part 3 Exercises

--- part3/exercises/src/components/Projects.jsx
+++ part3/exercises/src/components/Projects.jsx
@@ -1,18 +1,17 @@
-import data from '../data.json';
+import data from './data.json';
 import { useState } from 'react';
 
+
 export default function MyProjects() {
-   const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
   function handleClick() {
-   if (index < data.projects.length-1)
-   {
+    if (index < data.projects.length - 1) {
       setIndex(index + 1);
-   }
-   else
-   {
+    }
+    else {
       setIndex(0);
-   }
+    }
   }
 
   let projects = data.projects;
@@ -25,7 +24,7 @@ export default function MyProjects() {
       </button>
       <h2>{project.canvas} by {project.designer}
       </h2>
-      <img src={project.photoUrl} alt={project.alt} />
+      <img src={project.photoUrl} alt={project.alt} width="450px" />
     </div>
   );
 }
